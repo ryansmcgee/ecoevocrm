3f849fcb9126f014735a44998f494a371e806136
Ryan Seamus McGee <ryansmcgee@gmail.com>
2022-02-23 21:07:44 -0600

    Refactor StrainPool class (and strain_pool related variable names) to TypeSet (type_set); edited get_type_abundances; cleaned up print statements and commented-out lines

--- src/ecoevocrm/type_set.py
+++ src/ecoevocrm/type_set.py
@@ -5,7 +5,7 @@ import ecoevocrm.utils as utils
 #~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
 
 
-class StrainPool():
+class TypeSet():
 
     def __init__(self, num_types     = None,
                        num_traits    = None,
@@ -34,7 +34,7 @@ class StrainPool():
             self.num_types  = num_types
             self.num_traits = num_traits
         else:
-            utils.error("Error in StrainPool __init__(): Number of types and traits must be specified by providing a) a sigma matrix, or b) both num_types and num_traits values.")
+            utils.error("Error in TypeSet __init__(): Number of types and traits must be specified by providing a) a sigma matrix, or b) both num_types and num_traits values.")
 
         # Initialize type parameters:
         self.sigma = utils.reshape(sigma, shape=(self.num_types, self.num_traits))
@@ -62,8 +62,8 @@ class StrainPool():
                 new_lineage_id = self.add_type_to_phylogeny(i)
                 self.lineage_ids[i] = new_lineage_id
                 
-        #~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
-
+    
+    #~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
 
     def update_phenotypic_costs(self, sigma=None, c=None, chi=None, biochemistry=None):
         sigma = self.sigma if sigma is None else sigma
@@ -81,6 +81,8 @@ class StrainPool():
         return self.energy_costs
 
 
+    #~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
+
     def generate_mutant_phenotypes(self, sigma=None):
         sigma = self.sigma if sigma is None else sigma
         #----------------------------------
@@ -90,7 +92,9 @@ class StrainPool():
         return sigma_mut
 
 
-    def generate_mutant_pool(self, sigma=None, biochemistry=None, has_phylogeny=False):
+    #~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
+
+    def generate_mutant_set(self, sigma=None, biochemistry=None, has_phylogeny=False):
         sigma = self.sigma if sigma is None else sigma
         #----------------------------------
         sigma_mut = self.generate_mutant_phenotypes()
@@ -103,24 +107,26 @@ class StrainPool():
         chi_mut   = np.repeat(self.chi, repeats=sigma.shape[1], axis=0)
         mu_mut    = np.repeat(self.mu, repeats=sigma.shape[1], axis=0)
         #----------------------------------
-        mutant_pool = StrainPool(sigma=sigma_mut, b=b_mut, k=k_mut, eta=eta_mut, l=l_mut, g=g_mut, c=c_mut, chi=chi_mut, mu=mu_mut, has_phylogeny=has_phylogeny)
-        mutant_pool.update_phenotypic_costs(biochemistry=biochemistry)
+        mutant_set = TypeSet(sigma=sigma_mut, b=b_mut, k=k_mut, eta=eta_mut, l=l_mut, g=g_mut, c=c_mut, chi=chi_mut, mu=mu_mut, has_phylogeny=has_phylogeny)
+        mutant_set.update_phenotypic_costs(biochemistry=biochemistry)
         #----------------------------------
-        return mutant_pool
+        return mutant_set
 
 
-    def add_type(self, strain_pool=None, sigma=None, b=None, k=None, eta=None, l=None, g=None, c=None, chi=None, mu=None, index=None, parent_index=None, parent_id=None):
+    #~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
+
+    def add_type(self, type_set=None, sigma=None, b=None, k=None, eta=None, l=None, g=None, c=None, chi=None, mu=None, index=None, parent_index=None, parent_id=None):
         new_type_idx = index if index is not None else self.num_types # default to adding to end of matrices
         ref_type_idx = new_type_idx - 1
         parent_idx   = np.where(self.type_ids==parent_id)[0] if parent_id is not None else parent_index
         #----------------------------------
-        if(strain_pool is not None):
-            if(isinstance(strain_pool, StrainPool)):
-                new_strain_pool = strain_pool
+        if(type_set is not None):
+            if(isinstance(type_set, TypeSet)):
+                new_type_set = type_set
             else:
-                utils.error(f"Error in StrainPool add_type(): strain_pool argument expects object of StrainPool type.")
+                utils.error(f"Error in TypeSet add_type(): type_set argument expects object of TypeSet type.")
         else:
-            new_strain_pool = StrainPool(sigma=sigma if sigma is not None else self.sigma[ref_type_idx], 
+            new_type_set = TypeSet(sigma=sigma if sigma is not None else self.sigma[ref_type_idx], 
                                          b=b if b is not None else self.b[ref_type_idx],  
                                          k=k if k is not None else self.k[ref_type_idx],  
                                          eta=eta if eta is not None else self.eta[ref_type_idx],  
@@ -129,19 +135,19 @@ class StrainPool():
                                          c=c if c is not None else self.c[ref_type_idx],  
                                          chi=chi if chi is not None else self.chi[ref_type_idx],  
                                          mu=mu if mu is not None else self.mu[ref_type_idx])
-        # Check that the strain pool dimensions match the system dimensions:
-        if(self.num_traits != new_strain_pool.num_traits): 
-            utils.error(f"Error in StrainPool add_type(): The number of traits for added types ({new_strain_pool.num_traits}) does not match the number of strain pool traits ({self.num_traits}).")
+        # Check that the type set dimensions match the system dimensions:
+        if(self.num_traits != new_type_set.num_traits): 
+            utils.error(f"Error in TypeSet add_type(): The number of traits for added types ({new_type_set.num_traits}) does not match the number of type set traits ({self.num_traits}).")
         #----------------------------------
-        self.sigma = np.insert(self.sigma, new_type_idx, new_strain_pool.sigma, axis=0)
-        self.b     = np.insert(self.b,     new_type_idx, new_strain_pool.b,     axis=0)
-        self.k     = np.insert(self.k,     new_type_idx, new_strain_pool.k,     axis=0)
-        self.eta   = np.insert(self.eta,   new_type_idx, new_strain_pool.eta,   axis=0)
-        self.l     = np.insert(self.l,     new_type_idx, new_strain_pool.l,     axis=0)
-        self.g     = np.insert(self.g,     new_type_idx, new_strain_pool.g,     axis=0)
-        self.c     = np.insert(self.c,     new_type_idx, new_strain_pool.c,     axis=0)
-        self.mu    = np.insert(self.mu,    new_type_idx, new_strain_pool.mu,    axis=0)
-        self.chi   = np.insert(self.chi,   new_type_idx, new_strain_pool.chi,   axis=0)
+        self.sigma = np.insert(self.sigma, new_type_idx, new_type_set.sigma, axis=0)
+        self.b     = np.insert(self.b,     new_type_idx, new_type_set.b,     axis=0)
+        self.k     = np.insert(self.k,     new_type_idx, new_type_set.k,     axis=0)
+        self.eta   = np.insert(self.eta,   new_type_idx, new_type_set.eta,   axis=0)
+        self.l     = np.insert(self.l,     new_type_idx, new_type_set.l,     axis=0)
+        self.g     = np.insert(self.g,     new_type_idx, new_type_set.g,     axis=0)
+        self.c     = np.insert(self.c,     new_type_idx, new_type_set.c,     axis=0)
+        self.mu    = np.insert(self.mu,    new_type_idx, new_type_set.mu,    axis=0)
+        self.chi   = np.insert(self.chi,   new_type_idx, new_type_set.chi,   axis=0)
         #----------------------------------
         self.num_types = self.sigma.shape[0]
         #----------------------------------
@@ -149,13 +155,15 @@ class StrainPool():
         #----------------------------------
         self.type_ids = np.array([self.get_type_id(i) for i in range(self.num_types)])
         #----------------------------------
-        for i in range(new_type_idx, new_type_idx+new_strain_pool.num_types):
+        for i in range(new_type_idx, new_type_idx+new_type_set.num_types):
             new_lineage_id      = self.add_type_to_phylogeny(i, parent_idx, parent_id)
             self.lineage_ids    = np.insert(self.lineage_ids, i, new_lineage_id)
             self.parent_indices = np.insert(self.parent_indices, i, parent_idx)
         #----------------------------------
         return
 
+
+    #~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
 
     def add_type_to_phylogeny(self, new_type_index, parent_index=None, parent_id=None):
         parent_index = np.where(self.type_ids==parent_id)[0] if parent_id is not None else parent_index
@@ -178,16 +186,20 @@ class StrainPool():
         return new_lineage_id
 
 
+    #~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
+
     def get_type(self, index=None, type_id=None):
         # TODO: make possible to get multiple types by list of indices or ids
         if(index is None and type_id is None):
-            utils.error(f"Error in StrainPool get_type(): A type index or type id must be given.")
+            utils.error(f"Error in TypeSet get_type(): A type index or type id must be given.")
         type_idx = np.where(self.type_ids==type_id)[0] if type_id is not None else index
-        return StrainPool(sigma=self.sigma[type_idx], b=self.b[type_idx], k=self.k[type_idx], 
-                          eta=self.eta[type_idx], l=self.l[type_idx], g=self.g[type_idx], 
-                          c=self.c[type_idx], chi=self.chi[type_idx], mu=self.mu[type_idx],
-                          lineage_ids=np.array(self.lineage_ids[type_idx]).flatten())
+        return TypeSet(sigma=self.sigma[type_idx], b=self.b[type_idx], k=self.k[type_idx], 
+                        eta=self.eta[type_idx], l=self.l[type_idx], g=self.g[type_idx], 
+                        c=self.c[type_idx], chi=self.chi[type_idx], mu=self.mu[type_idx],
+                        lineage_ids=np.array(self.lineage_ids[type_idx]).flatten())
 
+
+    #~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
 
     def get_type_id(self, index):
         return hash(tuple( self.sigma[index].tolist() + self.b[index].tolist() + self.k[index].tolist() + self.eta[index].tolist()
--- src/ecoevocrm/viz.py
+++ src/ecoevocrm/viz.py
@@ -6,6 +6,48 @@ import seaborn as sns
 from matplotlib import rc
 rc('font',**{'family':'sans-serif','sans-serif':['Helvetica']})
 
+
+#~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
+
+def color_types_by_phylogeny(type_set, palette='hls', root_color='#AAAAAA', apply_palette_depth=1, shuffle_palette=True, color_step_start=0.11, color_step_slope=0.01, color_step_min=0.01):
+
+    # TODO: Allow for list of lineages that get excluded from having color palette applied (remain in grayscale)
+    # TODO: Allow for simple coloring of all types by rotating through given palette without subtree stuff, color of child being step away from parent, etc
+    # TODO: Make the range of random updates to child color based on phenotype or fitness difference between parent and child
+
+    num_palette_types = 0
+    for lineage_id in type_set.lineage_ids:
+        if(lineage_id.count('.') == apply_palette_depth):
+            num_palette_types += 1
+
+    palette = sns.color_palette(palette, num_palette_types)
+    if(shuffle_palette):
+        np.random.shuffle(palette)
+    
+    type_colors = [root_color for i in range(type_set.num_types)]
+    
+    def color_subtree(d, parent_color, depth, next_palette_color_idx):
+        if(not isinstance(d, dict) or not d):
+            return
+        parent_color_rgb   = tuple(int(parent_color.strip('#')[i:i+2], 16)/255 for i in (0, 2, 4)) if ('#' in parent_color and len(parent_color)==7) else parent_color
+        for lineage_id, descendants in d.items():
+            type_idx       = np.where(type_set.lineage_ids == lineage_id)[0][0]
+            if(depth == apply_palette_depth):
+                type_color = palette[next_palette_color_idx]
+                next_palette_color_idx += 1
+            elif(depth==0):
+                type_color = parent_color_rgb
+            else:
+                color_step_scale = max(color_step_start - color_step_slope*(depth-1), color_step_min)
+                type_color = tuple([np.clip((v + np.random.uniform(low=-1*color_step_scale, high=color_step_scale)), 0, 1) for v in parent_color_rgb])
+            type_colors[type_idx] = type_color
+            color_subtree(descendants, type_color, depth+1, next_palette_color_idx)
+        
+    color_subtree(type_set.phylogeny, parent_color=root_color, depth=0, next_palette_color_idx=0)
+
+    return type_colors
+
+
 #~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
 
 def matrix_plot(mat, ax=None, cmap=None, vmin=None, vmax=None, center=None, cbar=None, cbar_kws=None, cbar_ax=None,
@@ -30,50 +72,22 @@ def matrix_plot(mat, ax=None, cmap=None, vmin=None, vmax=None, center=None, cbar
                             cbar=cbar, cbar_kws=cbar_kws, cbar_ax=cbar_ax, square=square, 
                             xticklabels=xticklabels, yticklabels=yticklabels, mask=mask)
 
+
 #~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
 
-def stacked_abundance_plot(system, ax=None, palette='hls', root_color='#AAAAAA', 
-                           apply_palette_depth=0, shuffle_palette=True, color_step_start=0.1, color_step_slope=0.02, color_step_min=0.01,
+def stacked_abundance_plot(system, ax=None, type_colors=None, 
+                            palette='hls', root_color='#AAAAAA', apply_palette_depth=0, shuffle_palette=True, color_step_start=0.11, color_step_slope=0.01, color_step_min=0.01,
                            linewidth=None, edgecolor=None):
-    # TODO: Allow for list of lineages that get excluded from having color palette applied (remain in grayscale)
-    # TODO: Allow for simple coloring of all types by rotating through given palette without subtree stuff, color of child being step away from parent, etc
-    # TODO: Make the range of random updates to child color based on phenotype or fitness difference between parent and child
-    
-    num_palette_types = 0
-    for lineage_id in system.strain_pool.lineage_ids:
-        if(lineage_id.count('.') == apply_palette_depth):
-            num_palette_types += 1
 
-    palette = sns.color_palette(palette, num_palette_types)
-    if(shuffle_palette):
-        np.random.shuffle(palette)
-    
-    type_colors = [root_color for i in range(system.strain_pool.num_types)]
-    
-    def color_subtree(d, parent_color, depth, next_palette_color_idx):
-        if(not isinstance(d, dict) or not d):
-            return
-        parent_color_rgb   = tuple(int(parent_color.strip('#')[i:i+2], 16)/255 for i in (0, 2, 4)) if ('#' in parent_color and len(parent_color)==7) else parent_color
-        for lineage_id, descendants in d.items():
-            type_idx       = np.where(system.strain_pool.lineage_ids == lineage_id)[0][0]
-            if(depth == apply_palette_depth):
-                type_color = palette[next_palette_color_idx]
-                next_palette_color_idx += 1
-            elif(depth==0):
-                type_color = parent_color_rgb
-            else:
-                color_step_scale = color_step_start - min(color_step_slope*(depth-1), color_step_min)
-                type_color = tuple([np.clip((v + np.random.uniform(low=-1*color_step_scale, high=color_step_scale)), 0, 1) for v in parent_color_rgb])
-            type_colors[type_idx] = type_color
-            color_subtree(descendants, type_color, depth+1, next_palette_color_idx)
-        
-    color_subtree(system.strain_pool.phylogeny, parent_color=root_color, depth=0, next_palette_color_idx=0)
+    if(type_colors is None):
+        type_colors = color_types_by_phylogeny(system.type_set, palette=palette, root_color=root_color, apply_palette_depth=apply_palette_depth, shuffle_palette=shuffle_palette, color_step_start=color_step_start, color_step_slope=color_step_slope, color_step_min=color_step_min)
     
     ax = plt.axes() if ax is None else ax
 
     ax.stackplot(system.t_series, np.flip(system.N_series, axis=0), baseline='sym', colors=type_colors[::-1], linewidth=linewidth, edgecolor=edgecolor)
 
     return ax
+
 
 #~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
 
